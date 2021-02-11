76efda7a8ac7c2266296c910cffa44655684f160
Mathomike <71765642+Mathomike@users.noreply.github.com>
2021-02-11 14:13:02 +0100

    change

--- exercices/exercice1.py
+++ exercices/exercice1.py
@@ -1,2 +1,17 @@
-def tri_selection():
-    return ...
+def tri_selection(T):
+    for i in range(len(T)):
+        min = T[i]
+        for j in range(len(T)-i):
+            if min > T[j+i]:
+                min = T[j+i]
+                min_id = j+i
+            elif j == 0 :
+                min_id = j+i
+        if T[i] == T[min_id]:
+            pass
+        else :   
+            temp = T[min_id]
+            T[min_id] = T[i]
+            T[i] = temp 
+            
+    return T
